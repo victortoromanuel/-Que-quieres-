19dfee867a9aafda979b6cd98ddeb03cd5384449
victortoromanuel <victortoromanuel99@puj.edu.co>
2017-05-04 17:26:31 -0500

    Que quieres
    No se

---
+++ print.py
@@ -0,0 +1,10 @@
+def printa():
+    """
+    """
+    c = 0
+    while(c < 5):
+        print("Hola mundo")
+        if(c < 5):
+            print("¿Que quieres?")
+            printac = c + 1
+      
